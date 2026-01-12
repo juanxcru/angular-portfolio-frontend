97d69a9de5aa0742a08ffc29050bb47feeb90fa3
juanxcru <dinarjuanc@gmail.com>
2026-01-11 23:13:44 -0300

    first version: recalling angular + modern angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '**', redirectTo: 'home' },
+  {
+    path: 'home',
+    component: LandingComponent
+  },
+  {
+     path: '**', redirectTo: 'home'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('porftolio-FE');
+  protected readonly title = signal('portfolio-FE');
 }
