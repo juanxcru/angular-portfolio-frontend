6448a0cd9d2ce978ab4a1db158e418bc6a63e530
juanxcru <dinarjuanc@gmail.com>
2026-02-22 22:11:59 +0100

    about me component: first good approach

--- src/app/portfolio/components/portfolio-body/portfolio-body.component.ts
+++ src/app/portfolio/components/portfolio-body/portfolio-body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeroComponent } from "../hero/hero.component";
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-portfolio-body',
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent],
   templateUrl: './portfolio-body.component.html',
   styleUrl: './portfolio-body.component.css',
 })
