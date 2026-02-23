bd21fbcb39fb91dfdb50dce27a3b06e9060804f0
juanxcru <dinarjuanc@gmail.com>
2026-02-23 13:25:13 +0100

    experience, contact, projects: first approach + fixes

--- src/app/portfolio/components/contact/contact.component.ts
+++ src/app/portfolio/components/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactInfoComponent } from "../contact-info/contact-info.component";
+import { ContactFormComponent } from "../contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ContactInfoComponent, ContactFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
--- src/app/portfolio/components/portfolio-body/portfolio-body.component.ts
+++ src/app/portfolio/components/portfolio-body/portfolio-body.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeroComponent } from "../hero/hero.component";
 import { AboutComponent } from "../about/about.component";
+import { ExperienceComponent } from "../experience/experience.component";
+import { ProjectsComponent } from "../projects/projects.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-portfolio-body',
-  imports: [NavbarComponent, HeroComponent, AboutComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent, ExperienceComponent, ProjectsComponent, ContactComponent],
   templateUrl: './portfolio-body.component.html',
   styleUrl: './portfolio-body.component.css',
 })
